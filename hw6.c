24e4d5ce45faace5946bc6fac0f79e34c7e4b636
ojy0903 <ojy654@naver.com>
2021-11-08 21:08:58 +0900

    Add C Coding File [hw6.c]

---
+++ hw6.c
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void)
+{
+	int arr1[6] = { 1,2,3,4,5,6 };
+	int arr2[6] = { 7,8,9,10,11,12 };
+	int* ptr1 = &arr1[0];
+	int* ptr2 = &arr2[0];
+	int temp = 0, i;
+	printf("arr1: %d %d %d %d %d %d \n", arr1[0], arr1[1], arr1[2], arr1[3], arr1[4], arr1[5]);
+	printf("arr2: %d %d %d %d %d %d \n", arr2[0], arr2[1], arr2[2], arr2[3], arr2[4], arr2[5]);
+	for (i = 0; i < 6; i++)
+	{
+		temp = *ptr1;
+		*ptr1 = *ptr2;
+		*ptr2 = temp;
+		ptr1++;
+		ptr2++;
+	}
+	printf("\nafter swap\n");
+	printf("arr1: %d %d %d %d %d %d \n", arr1[0], arr1[1], arr1[2], arr1[3], arr1[4], arr1[5]);
+	printf("arr2: %d %d %d %d %d %d \n", arr2[0], arr2[1], arr2[2], arr2[3], arr2[4], arr2[5]);
+	return 0;
+}
